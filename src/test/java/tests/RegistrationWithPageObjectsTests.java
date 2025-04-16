e0c1f646748648dea3e8967b630f69c573a474b4
LizaRoma-lab <Elizaveta.Romanova.m@yandex.ru>
2025-04-16 21:41:42 +0300

    added tag for RegistrationWithPageObjectsTests

--- src/test/java/tests/RegistrationWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import tests.TestBase;
@@ -9,6 +10,7 @@ public class RegistrationWithPageObjectsTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
 
+    @Tag("simple")
     @Test
     void successfulRegistrationTest() {
 
