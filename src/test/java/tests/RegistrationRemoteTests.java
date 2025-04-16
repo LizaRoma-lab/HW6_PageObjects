28f1905eb4f20c156c3728e1dae92fd42ee4c866
LizaRoma-lab <Elizaveta.Romanova.m@yandex.ru>
2025-04-16 23:12:24 +0300

    added screenshot attach

--- src/test/java/tests/RegistrationRemoteTests.java
+++ src/test/java/tests/RegistrationRemoteTests.java
@@ -2,7 +2,9 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -33,6 +35,15 @@ public class RegistrationRemoteTests {
         ));
         Configuration.browserCapabilities = capabilities;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
     }
 
     @Test
