96405521f34cb3c4f1eb8a03a53e43923d53f756
LizaRoma-lab <Elizaveta.Romanova.m@yandex.ru>
2025-04-16 22:44:40 +0300

    added remote demoqa test

--- src/test/java/tests/RegistrationRemoteTests.java
+++ src/test/java/tests/RegistrationRemoteTests.java
@@ -19,6 +19,7 @@ public class RegistrationRemoteTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
