5932f1599d9c07940d4cc041ea90c06e9cedc00a
LizaRoma-lab <Elizaveta.Romanova.m@yandex.ru>
2025-04-17 22:56:55 +0300

    added property test

---
+++ src/test/java/properties/tests/SysytemPropertiesTests.java
@@ -0,0 +1,36 @@
+package properties.tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.sleep;
+
+public class SysytemPropertiesTests {
+
+    @Test
+    void systemProperties1Test() {
+        System.setProperty("browser", "chrome");
+        String browser = System.getProperty("browser");
+        System.out.println(browser);
+    }
+
+    @Test
+    void systemProperties2Test() {
+        System.setProperty("browser", "mozilla");
+        String browser = System.getProperty("browser");
+        System.out.println(browser);
+
+    }
+
+    @Test
+    @Tag("property")
+    void systemProperties3Test() {
+        String browser = System.getProperty("browser", "mozilla");
+        System.out.println(browser);
+        //gradle property_test
+        //mozilla
+
+        //gradle property_test -Dbrowser=opera
+        //opera
+    }
+}
--- src/test/java/tests/RegistrationWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -1,9 +1,7 @@
 package tests;
 
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
-import tests.TestBase;
 
 
 public class RegistrationWithPageObjectsTests extends TestBase {
